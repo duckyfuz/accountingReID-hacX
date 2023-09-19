e79fd8c0dba7d6b5a9b81d52b7ba10e4de4dd414
duckyfuz@iMac&cheese <108561447+duckyfuz@users.noreply.github.com>
2023-09-19 11:30:11 +0800

    Check deploy

--- django-backend/recognition/views.py
+++ django-backend/recognition/views.py
@@ -2,7 +2,7 @@ import json
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.response import Response 
 
 import numpy as np
 from PIL import Image
